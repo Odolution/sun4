62244c95185b292def0483682221ef2aaf6c4ae6
itisusamashahid <itisusamashahid@gmail.com>
2022-09-09 17:55:33 +0500

    updated_ol_project_sms_ext

--- ol_project_sms_ext/models/model.py
+++ ol_project_sms_ext/models/model.py
@@ -3,6 +3,29 @@ from odoo import models, api, fields, _
 from odoo.exceptions import UserError
 
 
+class twillioSMSExt(models.Model):
+    _inherit = 'twilio.sms.base'
+    project_id = fields.Many2one('project.project', string='project_id')
 class projectExt(models.Model):
     _inherit = 'project.project'
     
+    draft_subject = fields.Char('Subject')
+    draft_message = fields.Text('Body')
+    messages_ids = fields.One2many('twilio.sms.base', 'project_id', string='messages')
+    
+    def action_send_sms(self):
+        for rec in self:
+            if (not rec.draftMessage) or rec.draftMessage=="":
+                continue
+            
+            ids=[i.id for i in rec.message_partner_ids]
+            
+            rec.env['twilio.sms.base'].create({
+                'project_id':rec.id,
+                'body':rec.draftMessage,
+                'name':rec.draft_subject,
+                'multiple_member_ids':[(6, 0, ids)],
+            })
+        return
+    
+        
