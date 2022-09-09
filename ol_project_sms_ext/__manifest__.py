ef0ed63fd5b95fa89bc9f5cc65d1f7389c963647
itisusamashahid <itisusamashahid@gmail.com>
2022-09-09 16:26:41 +0500

    creating new module named ol_project_sms_ext

---
+++ ol_project_sms_ext/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'OL Project SMS Extension',
+
+    "author": "Usama Shahid",
+    'version': '0.1',
+    'category': 'Project',
+    'sequence': 95,
+    'summary': 'Custom Requirements of sun4 for twilio SMS and Project.',
+    'description': "Custom Requirements of sun4 for twilio SMS and Project.",
+    'website': '',
+    'images': [
+    ],
+    'depends': [
+        'tis_twilio_integration','project'
+        
+    ],
+    'data': [
+        
+        "views/project_ext.xml",
+        
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
---
+++ ol_project_sms_ext/models/model.py
@@ -0,0 +1,8 @@
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+
+
+class projectExt(models.Model):
+    _inherit = 'project.project'
+    
