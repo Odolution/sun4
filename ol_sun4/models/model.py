b053d8fc4fc83a118ee8046d241b68543e53edfc
Ali-Shahab <alishahab9090@gmail.com>
2023-01-12 11:38:19 +0500

    signature in delivery document

--- ol_sun4/models/model.py
+++ ol_sun4/models/model.py
@@ -7,6 +7,12 @@ from datetime import datetime,date
 class inheritintimesheet(models.Model):
     _inherit = 'account.analytic.line'
 
+class signaturedelivery(models.Model):
+    _inherit = 'stock.picking'
+
+    dispatcher_name = fields.Char(string='Name')
+    installation_signature = fields.Binary(string='Signature')
+
 
 
 
